2bf7be80e06d582210d35d22f91da9a03319871f
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2021-08-11 07:58:33 -0300

    Added blog component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 
-const AboutPage = () => {
+const BlogPage = () => {
     return (
         <Layout pageTitle="About Me">
             <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
@@ -14,4 +14,4 @@ const AboutPage = () => {
     )
 }
 
-export default AboutPage
+export default BlogPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,11 @@ import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
     return (
         <Layout pageTitle="Home Page">
-          <p>I'm making this by following the Gatsby Tutorial.</p>
-          <StaticImage
-            alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-            src="../images/baskets-1208280_960_720.jpg"
-          />
+            <p>I'm making this by following the Gatsby Tutorial.</p>
+            <StaticImage
+              alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+              src="../images/baskets-1208280_960_720.jpg"
+            />
         </Layout>
     )
 }
